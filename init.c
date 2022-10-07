a56069120f3e8adce001661d172190fdebb82032
Glenn Millon <gmillon@c4r2p2.42nice.fr>
2022-10-07 03:09:00 +0200

    fixed segfault on empty heredoc

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:35:18 by atrilles          #+#    #+#             */
-/*   Updated: 2022/10/03 19:33:30 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/10/07 03:08:28 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	init(t_command *command, int first)
 		free(command->options);
 	if (command->file_out && (!first))
 		free(command->file_out);
-	if (command->file_in && (!first))
-		free(command->file_in);
+	// if (command->file_in && (!first))
+	// 	free(command->file_in);
 	command->options = 0;
 	command->fd_file_in = 0;
 	command->file_in = 0;
